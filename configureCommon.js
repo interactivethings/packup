24e1c8545003150b52ce1d608b439e4059071f12
Jeremy Stucki <jeremy@interactivethings.com>
2015-08-26 12:22:02 +0200

    Use resolveLoader.root instead of moduleDirectories

--- configureCommon.js
+++ configureCommon.js
@@ -18,7 +18,7 @@ module.exports = function(options) {
       extensions: ['', '.js', '.json', '.css', '.scss']
     },
     resolveLoader: {
-      modulesDirectories: [
+      root: [
         path.resolve(__dirname, 'node_modules')
       ]
     },
