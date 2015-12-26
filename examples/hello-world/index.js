4bc4eb7f2e2392727727af8d267c44d7c6ee381d
Jeremy Stucki <jeremy@interactivethings.com>
2015-12-26 19:33:43 +0100

    Update dependencies

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,8 +1,9 @@
 // React is provided by packup
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 // the current directory is set as webpack module root, so we don't need to use relative paths for importing
 import App from 'components/App';
 
 // The index.html with an #app element is also provided by packup
-React.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
