19c68bacc3bae4fdc71d7e725ae2b36fd4c4dabc
Jeremy Stucki <jeremy@interactivethings.com>
2015-12-26 20:29:27 +0100

    Upgrade to Babel 6

--- configureDevelopment.js
+++ configureDevelopment.js
@@ -47,20 +47,23 @@ module.exports = function(options) {
       js: {
         loader: 'babel',
         query: {
+          presets: ['es2015', 'react', 'stage-2'],
           // Need to explicitly resolve to the local module
-          plugins: [path.resolve(__dirname, 'node_modules/babel-plugin-react-transform')],
-          extra: {
-            'react-transform': {
-              'transforms': [{
-                transform: path.resolve(__dirname, 'node_modules/react-transform-hmr'),
-                imports: ['react'],
-                locals: ['module']
-              }, {
-                transform: path.resolve(__dirname, 'node_modules/react-transform-catch-errors'),
-                imports: ['react', path.resolve(__dirname, 'node_modules/redbox-react')]
-              }]
-            }
-          }
+          plugins: [
+            [
+              path.resolve(__dirname, 'node_modules/babel-plugin-react-transform'),
+              {
+                transforms: [{
+                  transform: path.resolve(__dirname, 'node_modules/react-transform-hmr'),
+                  imports: ['react'],
+                  locals: ['module']
+                }, {
+                  transform: path.resolve(__dirname, 'node_modules/react-transform-catch-errors'),
+                  imports: ['react', path.resolve(__dirname, 'node_modules/redbox-react')]
+                }]
+              }
+            ]
+          ]
         }
       }
     }
